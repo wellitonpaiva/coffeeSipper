01c29e22972002b39110a6388d835232d831eb70
Welliton Portilho de Paiva <wellitonpaiva@gmail.com>
2018-01-12 23:21:24 -0200

    created main class and added test class

---
+++ src/test/java/br/com/thescientist/SipperTest.java
@@ -0,0 +1,9 @@
+package br.com.thescientist;
+
+import static org.junit.Assert.*;
+
+public class SipperTest {
+
+
+
+}
---
+++ src/test/java/hello/HelloWorldControllerTest.java
@@ -0,0 +1,42 @@
+package hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void sayHelloWithoutParametersTest() throws Exception {
+        this.mockMvc.perform(get("/hello-world"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, Stranger!")));
+    }
+
+    @Test
+    public void sayHelloWithParameterTest() throws Exception {
+        this.mockMvc.perform(get("/hello-world").param("name", "ton"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, ton!")));
+    }
+
+
+}
